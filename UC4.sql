00030afd7d9bc0be9b7d3b8fcce0ff18b34b08c5
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-04-12 07:32:24 +0530

    Added

---
+++ UC4.sql
@@ -0,0 +1,5 @@
+select * from employee_payroll
+insert into employee_payroll (name,salary,start) values
+('Anurag',10000,'2018-01-03'),
+('prakash',20000,'2017-03-09'),
+('Shanu',30000,'2011-04-10');
