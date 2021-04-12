000e74d596a20068154785d8d6741631f092be15
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-04-12 07:35:26 +0530

    Added

---
+++ UC5.sql
@@ -0,0 +1,7 @@
+select * from employee_payroll
+insert into employee_payroll (name,salary,start) values
+('Anurag',10000,'2018-01-03'),
+('prakash',20000,'2017-03-09'),
+('Shanu',30000,'2011-04-10');
+select salary from employee_payroll where name='Anurag'
+select start from employee_payroll where start between '2017' and GETDATE()
